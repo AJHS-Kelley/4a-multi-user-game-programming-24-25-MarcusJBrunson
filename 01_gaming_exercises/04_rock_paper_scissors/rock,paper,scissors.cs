b9a276a16b1fa3d1250ba685ac98c85f2a30f342
MarcusJBrunson <142345440+MarcusJBrunson@users.noreply.github.com>
2024-09-16 14:50:52 -0400

    Update rock,paper,scissors.cs

--- 01_gaming_exercises/04_rock_paper_scissors/rock,paper,scissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rock,paper,scissors.cs
@@ -2,45 +2,49 @@ using System;
 class Template {
   static void Main() {
 
-  string playerName = "TestPlayer";
-  int playerScore;
-  int cpuScore;
-  int rock = 0;
-  int paper = 1;
-  int scissors = 2;
-  Console.WriteLine("Your player name is " + playerName);
-  Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.\n The winner scores a point. If each player picks the same no points are awarded.\n The first player to five points wins.\n")
+    string playerName = "TestPlayer";
+    int playerScore;
+    int cpuScore;
+    int playerChoice;
+    int cpuChoice;
+    int rock = 0;
+    int paper = 1;
+    int scissors = 2;
+    // Rules for the Rock,Paper, and scissors games
+    Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.\n The winner scores a point. If each player picks the same no points are awarded.\n The first player to five points wins.\n")
+    Console.WriteLine("Your player name is")
+    Console.ReadLine("");
 
-  while (playerScore )
+    Random rnd = new Random(); 
+    int cpuChoice = rnd.Next(4);
+
+  while (playerScore || cpuScore != 5)
     {
-      // Console.WriteLine("Secret Number:" + secretNumber);
-      Console.WriteLine("Please guess an integer between 0 and 99.\n");
-      guess = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("Guess" + guess);
+      Console.WriteLine("Please select rock, paper, or scissors);
 
-      numGuess++;
-      if(guess < secretNumber)
-      {
-        Console.WriteLine("Your guess is too low.\n");
+       if(playerChoice = rock && cpuChoice = scissors)
+       {
+          Console.WriteLine("Rock beat Scissors.");
+          playerScore++; 
       }
-      else if (guess > secretNumber) 
-      {
-        Console.WriteLine("Your guess is too high.\n");
-      }
-      else
-      {
-        Console.WriteLine("You guessed correctly!.\n");
-        break; // Keyword to immeditaely exit a loop.
-      }
+       else if (playerChoice = paper && cpuChoice = rock) 
+       {
+         Console.WriteLine("Paper beat Rock.");
+         playerScore++;
+       }
+       else if (playerChoice = scissors && cpuChoice = paper)
+       {
+         Console.WriteLine("Scissors beat Paper.");
+         playerScore++;
+       }
 
-      if (numGuess > maxGuess)
-      {
-        Console.WriteLine("You have lost the game.\n your grandparents are dissapointed in you.\n");
-      }
+    //   if (numGuess > maxGuess)
+    //   {
+    //     Console.WriteLine("You have lost t he game.\n your grandparents are dissapointed in you.\n");
+       }
 
-    }
+     }
 
 
 
   }
-}

61dc410b71b8e66c352694bc03b370ae889ac936
MarcusJBrunson <142345440+MarcusJBrunson@users.noreply.github.com>
2024-09-20 14:08:03 -0400

    variables and else blocks for rock, paper, scissors

--- 01_gaming_exercises/04_rock_paper_scissors/rock,paper,scissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rock,paper,scissors.cs
@@ -1,26 +1,47 @@
 using System;
 class Template {
   static void Main() {
-
+    // PLAYER VARIABLES
     string playerName = "TestPlayer";
     int playerScore;
-    int cpuScore;
     int playerChoice;
+
+     // CPU VARIABLES
+    int cpuScore;
     int cpuChoice;
-    int rock = 0;
-    int paper = 1;
-    int scissors = 2;
-    
-    // Rules for Rock,Paper, and scissors game
-    Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.\n The winner scores a point. If each player picks the same no points are awarded.\n The first player to five points wins.\n")
-    
+
+    // int rock = 0;
+    // int paper = 1;
+    // int scissors = 2;
+
     // User inputs their name
-    Console.WriteLine("Your player name is")
-    Console.ReadLine();
-    
-    // CPU chioce is chosen at random 
-    Random rnd = new Random(); 
-    int cpuChoice = rnd.Next(4);
+    Console.WriteLine("Welcome to the rock paper, scissors bot!\n");
+    Console.WriteLine("Please type your player name and press ENTER.\n");
+    playerName = Console.ReadLine();
+    Console.WriteLine($"Hello {playerName}. is that correct?\n");
+    Console.WriteLine("Please type yes or no, then press enter.\n");
+    string isCorrect = Console.ReadLine().ToLower();
+
+    if (isCorrect == "yes")
+    {
+      Console.WriteLine($"Great! I'll call you {playerName}.\n");
+    }
+    else if (isCorrect == "no")
+    {
+      Console.WriteLine("Please type your player name and press ENTER.\n");
+      playerName = Console.ReadLine();
+    }
+    else
+    {
+      Console.WriteLine("Unable to determine your answer. Try again.\n");
+    }
+
+    // Rules for Rock, Paper, and scissors game
+    You will select from rock paper or Scissors.
+    The CPU will select one of those choices at random. 
+    Then a winner will be determined using the following rules
+    Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.\n The winner scores a point. If each player picks the same no points are awarded.\n The first player to five points wins.\n");
+
 
   while (playerScore || cpuScore != 5)
     {
@@ -30,7 +51,7 @@ class Template {
        {
           Console.WriteLine("Rock beat Scissors.\n");
           Console.WriteLine("Player has won.\n");
-          playerScore++; 
+          playerScore++;
       }
        else if (playerChoice = paper && cpuChoice = rock) 
        {

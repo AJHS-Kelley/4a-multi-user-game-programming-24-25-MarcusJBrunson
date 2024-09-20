5f8cff5c1af7cfb765d4e739047588801af77278
MarcusJBrunson <142345440+MarcusJBrunson@users.noreply.github.com>
2024-09-20 14:51:51 -0400

    Update rock,paper,scissors.cs

--- 01_gaming_exercises/04_rock_paper_scissors/rock,paper,scissors.cs
+++ 01_gaming_exercises/04_rock_paper_scissors/rock,paper,scissors.cs
@@ -3,12 +3,12 @@ class Template {
   static void Main() {
     // PLAYER VARIABLES
     string playerName = "TestPlayer";
-    int playerScore;
-    int playerChoice;
+    int playerScore = 0;
+    string playerChoice = "";
 
      // CPU VARIABLES
-    int cpuScore;
-    int cpuChoice;
+    int cpuScore = 0;
+    string cpuChoice = "";
 
     // int rock = 0;
     // int paper = 1;
@@ -37,53 +37,105 @@ class Template {
     }
 
     // Rules for Rock, Paper, and scissors game
-    You will select from rock paper or Scissors.
-    The CPU will select one of those choices at random. 
-    Then a winner will be determined using the following rules
-    Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.\n The winner scores a point. If each player picks the same no points are awarded.\n The first player to five points wins.\n");
+    Console.WriteLine("You will select from rock paper or Scissors.\n");
+    Console.WriteLine("The CPU will select one of those choices at random.\n");
+    Console.WriteLine("Then a winner will be determined using the following rules.\n");
+    Console.WriteLine("Rock beats Scissors, Scissors beats Paper, and Paper beats Rock.\n");
+    Console.WriteLine("The winner scores a point. If each player picks the same no points are awarded.\n");
+    Console.WriteLine("The first player to five points wins.\n");
 
 
-  while (playerScore || cpuScore != 5)
+  while (playerScore < 5 && cpuScore < 5)
     {
-      Console.WriteLine("Please select rock, paper, or scissors.\n");
-
-       if(playerChoice = rock && cpuChoice = scissors)
-       {
-          Console.WriteLine("Rock beat Scissors.\n");
-          Console.WriteLine("Player has won.\n");
-          playerScore++;
+      Console.WriteLine("Please select rock, paper, or scissors. Type your answer and press ENTER.\n");
+      Console.WriteLine($"Your score: {playerScore}\nCPU score: {cpuScore}/n");
+      playerChoice = COnsole.ReadLine().ToLower();
+      if (playerChoice != "rock" && playerChoice != "paper" & playerChoice != "scissors")
+      {
+        Console.WriteLine("Please select rock, paper, or scissors. Type your answer and press ENTER.\n");
+        playerChoice = Console.ReadLine().ToLower();
       }
-       else if (playerChoice = paper && cpuChoice = rock) 
-       {
-         Console.WriteLine("Paper beat Rock.\n");
-         COnsole.WriteLine("Player has won.\n");
-         playerScore++;
-       }
-       else if (playerChoice = scissors && cpuChoice = paper)
-       {
-         Console.WriteLine("Scissors beat Paper.\n");
-         Console.WriteLine("Player has won.\n");
-         playerScore++;
-       }
+      // Allow CPU to select randomly
+      Random rnd = new random();
+      int cpuRand = rnd.Next(0,2)
 
-       if(cpuChoice = rock && playerChoice = scissors)
-       {
-          Console.WriteLine("Rock beat Scissors.\n");
-          Console.WriteLine("CPU has won.\n");
-          cpuScore++; 
+      if (cpuRand == 0)
+      {
+        cpuChoice = "rock";
       }
-       else if (cpuChoice = paper && playerChoice = rock) 
-       {
-         Console.WriteLine("Paper beat Rock.\n");
-         Console.WriteLine("CPU has won.\n");
-         cpuScore++;
-       }
-       else if (cpuChoice = scissors && playerChoice = paper)
-       {
-         Console.WriteLine("Scissors beat Paper.\n");
-         Console.WriteLine("CPU has won.\n");
-         cpuScore++;
-       }
+      else if (cpuRand == 1)
+      {
+        cpuChoice = "paper";
+      }
+      else if (cpuRand == 2)
+      {
+        cpuChoice = "scissors"
+      }
+      else
+      {
+        Console.WriteLine("Unable to determine CPU choice.\n");
+      }
+      Console.WriteLine("CPU choice" + cpuChoice);
+
+      // Compare the two choices and determine a winner.
+      if(playerChoice == "rock" && cpuChoice == "paper")
+      {
+        Console.WriteLine($"you chose {playerchoice} and the CPU chose {cpuChoice}.\n");
+        Console.WriteLine("The CPU wins.\n");
+        cpuScore++;
+      }
+      else if (playerChoice == "rock" && cpuChoice == "scissors")
+      {
+        Console.WriteLine($"you chose {playerchoice} and the CPU chose {cpuChoice}.\n");
+        Console.WriteLine("You win.\n");
+        playerScore++;
+      }
+      else if  (playerChoice == "rock" && cpuChoice == "rock")
+      {
+        Console.WriteLine($"you chose {playerchoice} and the CPU chose {cpuChoice}.\n");
+        Console.WriteLine("It's a draw.\n");
+      }
+
+
+      
+
+      //  if(playerChoice = rock && cpuChoice = scissors)
+      //  {
+      //     Console.WriteLine("Rock beat Scissors.\n");
+      //     Console.WriteLine("Player has won.\n");
+      //     playerScore++;
+      // }
+      //  else if (playerChoice = paper && cpuChoice = rock) 
+      //  {
+      //    Console.WriteLine("Paper beat Rock.\n");
+      //    COnsole.WriteLine("Player has won.\n");
+      //    playerScore++;
+      //  }
+      //  else if (playerChoice = scissors && cpuChoice = paper)
+      //  {
+      //    Console.WriteLine("Scissors beat Paper.\n");
+      //    Console.WriteLine("Player has won.\n");
+      //    playerScore++;
+      //  }
+
+      //  if(cpuChoice = rock && playerChoice = scissors)
+      //  {
+      //     Console.WriteLine("Rock beat Scissors.\n");
+      //     Console.WriteLine("CPU has won.\n");
+      //     cpuScore++; 
+      // }
+      //  else if (cpuChoice = paper && playerChoice = rock) 
+      //  {
+      //    Console.WriteLine("Paper beat Rock.\n");
+      //    Console.WriteLine("CPU has won.\n");
+      //    cpuScore++;
+      //  }
+      //  else if (cpuChoice = scissors && playerChoice = paper)
+      //  {
+      //    Console.WriteLine("Scissors beat Paper.\n");
+      //    Console.WriteLine("CPU has won.\n");
+      //    cpuScore++;
+      //  }
 
        }
      }

c6eb70c4c3f09e45b69eacd0fe68f946d51a92bb
MarcusJBrunson <142345440+MarcusJBrunson@users.noreply.github.com>
2024-11-01 13:49:20 -0400

    Update templae.cs

--- 00_computer_science_exercises/04_collections/templae.cs
+++ 00_computer_science_exercises/04_collections/templae.cs
@@ -3,30 +3,56 @@ class Collections {
   static void Main() { // DO NOT DELETE!
 
 
+
+
+/*
+In C# an collection is an ARRAY
+
+
+
+
+
+
+
+*/
+
   // Declaring and defending an array 
-  string [] breakfastfoods = {"Bacon", "Eggs", "Cold Pizza", "Apple jacks", "Oatmeal"};
-  int[] testscore = {0, 25, 15, 75, 99};
+  string [] breakfastFoods = {"Bacon", "Eggs", "Cold Pizza", "Apple jacks", "Oatmeal"};
+  int[] testScores = {0, 25, 15, 75, 99};
   double[] GPA = {0.27D, 1.99D, 4.02D, 0.0D, 3.25D};
 
   // Print Array Contents
   Console.WriteLine("The elements of each array are;\n");
-  Console.WriteLine("breakfastfoods:\n" + String.Join(",", breakfastfoods));
+  Console.WriteLine("breakfastFoods:\n" + String.Join(",", breakfastFoods));
   Console.WriteLine(); // Print an empty line to the screen
-
+/*
   // Print Array Contents
   Console.WriteLine("The elements of each array are:\n");
-  Console.WriteLine("breakfastfoods:\n" + String.Join("\n", breakfastfoods));
+  Console.WriteLine("breakfastFoods:\n" + String.Join("\n", breakfastFoods));
   Console.WriteLine(); // Print an empty line to the screen
 
   // How long is my array? DETERMING ARRAY LENGTH
-  Console.WriteLine("The length of the breakfastfoods array is: " + breakfastfoods.Length);
+  Console.WriteLine("The length of the breakfastFoods array is: " + breakfastFoods.Length);
   // .Length is known as PROPERTY of the array
 
   // Acessing Elements in the array with the index.
-  Console.WriteLine("The first element in breakfastfoods is: " + breakfastfoods[0]);
-  Console.WriteLine("The second element in testscores is: " + testscore[1]);
+  Console.WriteLine("The first element in breakfastFoods is: " + breakfastFoods[0]);
+  Console.WriteLine("The second element in testScores is: " + testScores[1]);
   Console.WriteLine("The thrid element in GPA is: " + GPA[2]);
 
+  // Shortcut to Access last item in an array 
+  // Console.WriteLine("The last element of breakfastFoods is: " + breakfastFoods[breakfastFoods.Length -1]);
+  // Console.WriteLine("The last element of testScores is: " + testScores[testScores.Length -1]);
+  // Console.WriteLine("The last element of GPA is: " + GPA[GPA.Length -1]);
+
+  // CHANGING ELEMENTS IN THE ARRAY
+  breakfastFoods[2] = "Frosted Flakes";
+  testScores[0] = -5;
+  GPA[1] = 1.3;
+  Console.WriteLine("The elements of each array are;\n");
+  Console.WriteLine("breakfastFoods:\n" + String.Join(",", breakfastFoods));
+  Console.WriteLine(); // Print an empty line to the screen
+*/
 
     } // DO NOT DELETE EVER, 3 SPACES INDENTED FROM LEFT
 } // DO NOT DELETE EVER, ALWAYS TOUCHING LRFT MARGIN
